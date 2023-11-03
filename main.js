1461348d8024e5c94045847e405b2cd578e7a890
prinsi-t <tprincy56@gmail.com>
2023-11-03 22:06:57 +0530

    made chnges

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 const searchBox = document.querySelector('.searchBox')
 const searchBtn = document.querySelector('.searchBtn')
-const searchResult = document.querySelector('.searchResult')
+const recipeContainer = document.querySelector('.recipeContainer')
 
 const getRecipes = async (query) => {
+    recipeContainer.innerHTML = '<h2>Fetching Your Recipes....</h2>';
     const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
     const response = await data.json();
     
+    recipeContainer.innerHTML = '';
     response.meals.forEach(meal => {
-        console.log(meal);
+        const recipe = document.createElement('div');
+        recipe.classList.add('recipe');
+        recipe.innerHTML = `
+            <img class='img' src='${meal.strMealThumb}'>
+            <h3>${meal.strMeal}</h3>
+            <p>${meal.strArea}</p>
+            <button>${}
+        `
+        recipeContainer.appendChild(recipe);
+        //console.log(meal);
     });
    
 }
